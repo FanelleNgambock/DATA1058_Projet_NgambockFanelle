28568cac69c12af13335832700bcc1d12ad26a8a
Fanelle <ngambocky@gmail.com>
2023-05-26 04:55:25 -0300

    envoie des informations au serveur python, sauvegarde des données dans la base de données, ajout de l'adresse mac dans les informations reçues

--- Projet_DATA1058/src/main.cpp
+++ Projet_DATA1058/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "Adafruit_CCS811.h"
 #include <SPI.h>
+#include <HTTPClient.h>
 
 #include "Humidity.hpp"
 #include "Led.hpp"
@@ -8,6 +9,10 @@
 #include "Steam.hpp"
 // declaration des variables
 
+
+const char* ssid = "FibreOP532";
+const char* password = "9PXPE66PM6XM55M8";
+const char* serverName = "http://192.168.2.29:8000";
 Adafruit_CCS811 ccs;
 
 Steam mySteamCaptor(39);
@@ -21,6 +26,16 @@ int niveauHum = 0;
 
 void setup() {
   Serial.begin(9600);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 
   //capteur de CO2
   if(!ccs.begin()){
@@ -29,31 +44,23 @@ void setup() {
   }
   // Attente que le capteur soit pret ...
   while(!ccs.available());
-
   //capteur de vapeur d'eau
   mySteamCaptor.setupcaptor();
-
   //capteur d'humidite
   myHumidity.setupSoil();
-
   //capteur de luminosite
   lumCaptor.setup();
-
   //led
   mesLed.setup();
-
 }
 void loop() {
 
   //capteur de vapeur d'eau
-  niveauVap = mySteamCaptor.steamLecture();
-
+  niveauVap = (mySteamCaptor.steamLecture() * 100) /4095;
   //capteur d'humidite
   niveauHum = myHumidity.humidityLecture();
-
   //capteur de luminosite
-  niveauLum = (lumCaptor.detection() * 4095) /100;
-
+  niveauLum = (lumCaptor.detection() * 100) /4095;
   if(niveauHum < 2002)
   {
     mesLed.onBlue();
@@ -74,28 +81,54 @@ void loop() {
   //capteur de CO2
   if(ccs.available()){
     if(!ccs.readData()){
-      // Serial.print("CO2 : ");
-      // Serial.print(ccs.geteCO2());
-      // Serial.print("ppm, TVOC : ");
-      // Serial.println(ccs.getTVOC());
+      Serial.print("CO2 : ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC : ");
+      Serial.println(ccs.getTVOC());
     }
     else{
       Serial.println("Erreur !");
       while(1);
     }
   }
-
-  // Serial.print("le niveau de vapeur d'eau degagee est : ");
-  // Serial.println(niveauVap);
-
-  // Serial.print("le niveau d'humidite du sol est : ");
-  // Serial.println(niveauHum);
+  Serial.print("le niveau de vapeur d'eau degagee est : ");
+  Serial.println(niveauVap);
+  Serial.print("le niveau d'humidite du sol est : ");
+  Serial.println(niveauHum);
 
   Serial.print("la valeur de luminosite est : ");
   Serial.print(niveauLum);
   Serial.println(" %");
 
+  // Création du message à envoyer au serveur
+  String httpDATA = "mac_address=";
+  httpDATA += WiFi.macAddress();
+  httpDATA += "&vapeurEau=";
+  httpDATA += niveauVap;
+  httpDATA += "&luminosite=";
+  httpDATA += niveauLum;
+  httpDATA += "&humidite=";
+  httpDATA += niveauHum;
+  httpDATA += "&co2=";
+  httpDATA += ccs.geteCO2();   
+
+
   // Serial.println("-------------------------------------");
+  if(WiFi.status()== WL_CONNECTED){
+      // Envoie du message POST au serveur
+      HTTPClient http;
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverName);      
+      http.addHeader("Content-Type", "text/plain");
+      int httpResponseCode = http.POST(httpDATA);
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
 
   delay(5000);
 
--- Python code/server.py
+++ Python code/server.py
@@ -3,13 +3,14 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from flask import Flask, request
 import time
 import psycopg2
+import urllib.parse
 
 hostName = "localhost"
 serverPort = 1234
 # Informations de connexion à la base de données PostgreSQL
 db_host = "localhost"
 db_port = 5432
-db_name = "AIOTCanadaTest" 
+db_name = "suivi_croissance" 
 db_user = "postgres"
 db_password = "admin"
 
@@ -22,7 +23,24 @@ def handle_request():
     elif request.method == 'POST':
         data = request.get_data(as_text=True)
         print(data)
-        '''
+
+        # Analyse de la chaîne de requête
+        parsed_query = urllib.parse.parse_qs(data)
+
+        # Extraction des valeurs
+        mac_address = parsed_query.get('mac_address', [None])[0]
+        vapeurEau = int(parsed_query.get('vapeurEau', [0])[0])
+        luminosite = int(parsed_query.get('luminosite', [0])[0])
+        humidite = int(parsed_query.get('humidite', [0])[0])
+        co2 = int(parsed_query.get('co2', [0])[0])
+
+        # Affichage des valeurs extraites
+        print("MAC address:", mac_address)
+        print("Vapeur d'eau:", vapeurEau)
+        print("Luminosité:", luminosite)
+        print("Humidité:", humidite)
+        print("CO2:", co2)
+        
         # Connexion à la base de données PostgreSQL
         conn = psycopg2.connect(
             host=db_host,
@@ -35,15 +53,15 @@ def handle_request():
         cursor = conn.cursor()
 
         # Exemple d'insertion des données dans une table nommée "temperature_data"
-        insert_query = "INSERT INTO telemetrie (valeur) VALUES (%s)"
-        cursor.execute(insert_query, (data,))
+        insert_query = "INSERT INTO record (mac_address, vapeur_eau, luminosite, humidite, co2_value) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_query, (mac_address, vapeurEau, luminosite, humidite, co2,))
         conn.commit()
 
         # Fermeture de la connexion à la base de données
         cursor.close()
         conn.close()
         # Traitez les données reçues du microcontrôleur ici
-        '''
+        
         return 'Données reçues...'
 
 if __name__ == '__main__':
